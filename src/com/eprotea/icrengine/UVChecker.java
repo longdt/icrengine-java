025f41ecb5c2c20d1c4ddc93a5a3a27cae0631f3
longdt <longdt@localhost>
2015-04-14 11:44:45 +0700

    add java doc

--- src/com/eprotea/icrengine/UVChecker.java
+++ src/com/eprotea/icrengine/UVChecker.java
@@ -4,5 +4,12 @@ public class UVChecker {
 	static {
 		NativeUtils.loadLibraries("uvanalyzer");
 	}
+	
+	/**
+	 * check whether a given uvImg is valid or not
+	 * @param uvImg
+	 * @return true if uvImg is valid
+	 * @throws ICRException
+	 */
 	public static native boolean checkValid(byte[] uvImg) throws ICRException;
 }
