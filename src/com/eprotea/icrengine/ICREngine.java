bc12969622bd7c84a57ca2425ddcddaf6dfecc10
longdt <longdt@localhost>
2015-05-19 15:32:16 +0700

    support cheque type

--- src/com/eprotea/icrengine/ICREngine.java
+++ src/com/eprotea/icrengine/ICREngine.java
@@ -2,6 +2,10 @@ package com.eprotea.icrengine;
 
 
 public class ICREngine {
+	public static final int CHEQUE_MY = 0;
+	public static final int CHEQUE_PH = 1;
+	private int chqType;
+	
 	static {
 		try {
 			NativeUtils.loadLibraries("icrengine");
@@ -16,11 +20,15 @@ public class ICREngine {
 	 */
 	public static native void loadModels(String modelsFolder) throws ICRException;
 	
+	public ICREngine(int chqType) {
+		this.chqType = chqType;
+	}
+
 	public Result predictCA(byte[] chqImg) throws ICRException {
 		double[] output = new double[2];
-		predictCA(chqImg, output);
+		predictCA(chqType, chqImg, output);
 		return new Result(output[0], output[1]);
 	}
 	
-	private native void predictCA(byte[] chqImg, double[] output) throws ICRException;
+	private native void predictCA(int chqType, byte[] chqImg, double[] output) throws ICRException;
 }
